cf046e28135f243319f7a661d67af74c84b0f72b
Tyfee <zetsubou1403@gmail.com>
2026-03-16 12:13:26 -0300

    Remove build files from repository

--- CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/Users/tyfe1/Projetos/lang_it_compiler
-# Build directory: /mnt/c/Users/tyfe1/Projetos/lang_it_compiler
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(help_test "/mnt/c/Users/tyfe1/Projetos/lang_it_compiler/bin/compiler")
-set_tests_properties(help_test PROPERTIES  WILL_FAIL "TRUE" _BACKTRACE_TRIPLES "/mnt/c/Users/tyfe1/Projetos/lang_it_compiler/CMakeLists.txt;37;add_test;/mnt/c/Users/tyfe1/Projetos/lang_it_compiler/CMakeLists.txt;0;")
